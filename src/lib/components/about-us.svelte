08c8229927e268d18895ab4e72f3512d3ec2c4b2
maxime Surmont LCDM <maxime.surmont@lechemindesmures.fr>
2022-12-20 23:03:32 +0100

    🚀 implement about us page

--- src/lib/components/about-us.svelte
+++ src/lib/components/about-us.svelte
@@ -1,7 +1,21 @@
 <script>
 	import PageLayout from './page-layout.svelte';
+	import PhotoCarousel from './photo-carousel.svelte';
 	import Spacer from './spacer.component.svelte';
+
+	let photos = ['us-street.jpg', 'us-street-2.jpg', 'us-canada.JPG', 'us-hands.jpg'];
+	let displayCarousel = false;
+	let photoToDisplayIndex = '';
+
+	const setDisplayCarousel = (photoToSet) => {
+		displayCarousel = true;
+		photoToDisplayIndex = photoToSet;
+	};
 </script>
+
+{#if displayCarousel}
+	<PhotoCarousel bind:photos bind:photoToDisplayIndex bind:displayCarousel />
+{/if}
 
 <PageLayout>
 	<svelte:fragment slot="header-content">
@@ -13,11 +27,20 @@
 
 	<svelte:fragment slot="main-content">
 		<div class="content-container flex-centered p-10">
-			<!-- svelte-ignore a11y-media-has-caption -->
-			<!-- <video autoplay loop class="shadow">
-				<source src="about-us.mp4" type="video/mp4" />
-				Your browser does not support the video tag.
-			</video> -->
+			<div class="pictures-container flex-centered">
+				<img
+					class="picture-1"
+					src={photos[1]}
+					alt="chloé et maxime"
+					on:click={() => setDisplayCarousel(1)}
+				/>
+				<img
+					class="picture-2"
+					src={photos[0]}
+					alt="chloé et maxime"
+					on:click={() => setDisplayCarousel(0)}
+				/>
+			</div>
 
 			<Spacer />
 			<p class="text-centered">
@@ -29,6 +52,13 @@
 				A notre retour du Canada en 2018, nous emménageons dans un charmant appartement lillois dans
 				lequel nous avons continué notre petite vie.
 			</p>
+			<img
+				class="picture-3"
+				src={photos[2]}
+				alt="chloé et maxime"
+				on:click={() => setDisplayCarousel(2)}
+			/>
+
 			<p class="text-centered">
 				Après cette expérience concluante et 2 CDI en poche, nous achetons notre maison à Lille !
 				Nous y avons fait couler beaucoup de sueurs pour en faire notre petit cocon. Confinés mais
@@ -42,11 +72,47 @@
 				En bref, vous êtes invité au mariage d’une jeune Picarde et d’un beau Lillois qui filent le
 				parfait amour depuis un peu plus de 7 ans.
 			</p>
+			<img
+				class="picture-4"
+				src={photos[3]}
+				alt="chloé et maxime"
+				on:click={() => setDisplayCarousel(3)}
+			/>
+			<p />
 		</div>
 	</svelte:fragment>
 </PageLayout>
 
 <style>
+	.pictures-container {
+		position: relative;
+	}
+
+	img {
+		width: 50%;
+		border: 5px solid white;
+		box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+		cursor: zoom-in;
+	}
+
+	.picture-1 {
+		transform: rotate(7deg);
+		position: absolute;
+	}
+
+	.picture-2 {
+		transform: rotate(-5deg);
+	}
+
+	.picture-3 {
+		transform: rotate(3deg);
+	}
+
+	.picture-4 {
+		width: 80%;
+		transform: rotate(5deg);
+	}
+
 	video {
 		width: 50%;
 		width: 300px;
---
+++ src/lib/components/photo-carousel.svelte
@@ -0,0 +1,72 @@
+<script>
+	import { scale } from 'svelte/transition';
+
+	export let photos;
+	export let displayCarousel;
+	export let photoToDisplayIndex;
+
+	const setPreviousPicture = () => {
+		if (photoToDisplayIndex === 0) {
+			photoToDisplayIndex = photos.length - 1;
+		} else {
+			photoToDisplayIndex -= 1;
+		}
+	};
+
+	const setNextPicture = () => {
+		if (photoToDisplayIndex + 1 === photos.length) {
+			photoToDisplayIndex = 0;
+		} else {
+			photoToDisplayIndex += 1;
+		}
+	};
+</script>
+
+<div class="pictures-container flex-centered" transition:scale>
+	<span class="text-white cursor-pointer">
+		<span class="close-icon" on:click={() => (displayCarousel = false)}>fermer</span>
+	</span>
+
+	<img src={photos.at(photoToDisplayIndex)} alt="chloé et maxime" />
+	<span class="text-white cursor-pointer">
+		<span class="previous-icon" on:click={setPreviousPicture}>précédente</span>
+		<span class="next-icon" on:click={setNextPicture}>suivante</span>
+	</span>
+</div>
+
+<style>
+	span > span {
+		padding: 5px;
+	}
+
+	.close-icon {
+		background-color: var(--red);
+		z-index: 5;
+	}
+
+	.previous-icon {
+		background-color: var(--pink);
+	}
+
+	.next-icon {
+		background-color: var(--yellow);
+	}
+
+	.pictures-container {
+		width: 100vw;
+		height: calc(100vh - 58px);
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: 3;
+		backdrop-filter: blur(6px);
+	}
+
+	img {
+		z-index: -1;
+		width: 410px;
+		max-width: 90%;
+		border: 10px solid white;
+		box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+	}
+</style>
